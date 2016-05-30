e0bc36944f9badccd237e422e2cadf4fd8fc89c0
Scott Wallace <scott@wallace.sh>
2016-05-30 10:28:20 +0100

    Check in BINDv9 config.

---
+++ generate-forwards.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOMAINS=/etc/bind/domain-list
+BIND_FILE=/etc/bind/named.conf.local
+
+while read DOMAIN; do
+    echo "zone \"${DOMAIN}\" IN {"
+    echo "    type forward;"
+    echo "    forwarders {"
+    echo "        185.37.37.37;   // Unlocator DNS"
+    echo "    };"
+    echo "};"
+done < ${DOMAINS} > ${BIND_FILE}
